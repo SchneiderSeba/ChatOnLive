e3711c7eaea32984d0b34de1eb72ff41099c3df3
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-25 22:10:37 +0200

    Client UI and Socket messeges working

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,34 @@
 import express from 'express'
+import morgan from 'morgan'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-    res.send('<h1>HOME</h1>')
+io.on('connection', (socket) => {
+    console.log('A user connected')
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
 })
 
-app.listen(PORT, () => {
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/Client/index.html')
+})
+
+server.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
 })
 
