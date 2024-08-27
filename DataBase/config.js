01c70e53fdd0eb2d27c88beb39f053da832dcddf
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-27 21:13:19 +0200

    ,k

--- DataBase/config.js
+++ DataBase/config.js
@@ -1,5 +1,5 @@
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '15673019'
-export const DB_NAME = process.env.DB_NAME || 'ChatDB'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'MncdLuGNAdYEoGMxFnlLkecIqbseJojB'
+export const DB_NAME = process.env.DB_NAME || 'railway'
 export const DB_PORT = process.env.DB_PORT || 3306
