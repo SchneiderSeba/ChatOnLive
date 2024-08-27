5c2453dbd4760f00dcab9d85ed723395c1d261a7
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-27 17:07:31 +0200

    Database

---
+++ DataBase/ChatDB.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import mysql from 'mysql2/promise'
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
+
+const config = {
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  connectTimeout: 10000
+}
+
+ export const connection = await mysql.createConnection(config)
--- Server/index.js
+++ Server/index.js
@@ -2,12 +2,23 @@ import express from 'express'
 import morgan from 'morgan'
 import { createServer } from 'node:http'
 import { Server } from 'socket.io'
+import { connection } from '../DataBase/ChatDB.js'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
+
+await connection.query(
+    `CREATE TABLE IF NOT EXISTS messages (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    content TEXT, 
+    )`
+)
+
 
 io.on('connection', (socket) => {
     console.log('A user connected')
